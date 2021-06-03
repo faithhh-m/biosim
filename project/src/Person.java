f8f0e7c77d92b17b3ba48f41f93830f2c6405440
faithhh-m <85309857+faithhh-m@users.noreply.github.com>
2021-06-03 10:47:29 -0700

    Add files via upload

---
+++ project/src/Person.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Person {
+    private int energy;
+    private double x;
+    private double y;
+    private ArrayList<Person> bobs;
+    private boolean alive = true;
+    private Color bobcolor;
+    private int age;
+    private final double size = 15;
+    
+    public Person(double x, double y, ArrayList<Person> bobs){
+        energy=500;
+        this.x=x;
+        this.y=y;
+        this.bobs=bobs;
+        alive = true;
+        bobcolor=Color.CYAN;
+        age=0;
+    }
+    
+    public int getEnergy(){
+        return energy;
+    }
+    
+    public boolean alive(){
+        return alive;
+    }
+    
+    public void dead(){
+        alive = false;
+    }
+    
+    public int distance(Person bob){
+        int distance = 0; //fix this!!!
+        return distance;
+    }
+    
+    //will check if bob has bumped into food. if so, eat food
+    public void bump(){
+        for(int k=0;k<bobs.size()-1;k++){
+            Person thisBob = bobs.get(k);
+            //requires the distance method
+        }
+    }
+    
+    //this method will change the speed of the bobs depending on their age
+    public void age(){
+        for(int b=0; b<bobs.size(); b++){
+            
+        }
+    }
+    
+    public void energyLevel(){
+        for(int j=0; j<bobs.size(); j++){
+            if(energy>1000){
+            //add code to drop trail of food here
+            bobs.get(j);
+            }
+            if(energy==0){
+                dead();
+            }
+            else if(energy<=100){
+                bobcolor=Color.RED;
+            }
+            else{
+                bobcolor=Color.CYAN;
+            }
+        }
+        
+    }
+    
+}
