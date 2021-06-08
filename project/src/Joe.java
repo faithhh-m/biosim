122852c592c56fdacc293f1f904394b0c266202b
faithhh-m <85309857+faithhh-m@users.noreply.github.com>
2021-06-08 10:39:56 -0700

    Add files via upload

---
+++ project/src/Joe.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.awt.Color;
+
+public class Joe extends Person{
+    
+    public Joe(double x, double y, ArrayList<Person> bobs){
+        super(x,y,bobs);
+        newColor(Color.MAGENTA);
+    }
+    
+    @Override
+    public void doSomething(){
+        move();
+        energyLevel();
+        eat();
+    }
+    
+    @Override
+    public void energyLevel(){
+        decreaseEnergy();
+        for(int k=0; k<bobs.size(); k++){
+            if(bobs.get(k).getEnergy()==0){
+                dead();
+            }
+            else if(bobs.get(k).getEnergy()<=100){
+                bobs.get(k).newColor(Color.DARK_GRAY);
+            }
+        }
+    }
+    
+}
