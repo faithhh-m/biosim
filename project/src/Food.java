260154c21a42ef9c857b683a7390546fb27f03e0
Joyce Zhang <43057355+JoJozz@users.noreply.github.com>
2021-06-08 10:13:25 -0700

    Add files via upload

---
+++ project/src/Food.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Food extends Person {
+    
+    
+    
+    public Food(double x, double y, ArrayList<Person> foods){
+        super(x, y, foods);
+        
+    }
+    
+    
+    
+    
+    
+}
