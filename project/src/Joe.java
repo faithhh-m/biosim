30bf75b14db2e2fdf7ae00db83bffc86f3beef9e
faithhh-m <85309857+faithhh-m@users.noreply.github.com>
2021-06-10 10:00:15 -0700

    Add files via upload

---
+++ project/src/Joe.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.awt.Color;
+
+public class Joe extends Person{
+    
+    public Joe(double x, double y, ArrayList<Person> bobs){
+        super(x,y,bobs);
+        newColor(Color.MAGENTA);
+    }
+    
+    public void spawn(){
+        if(getAge()>=3){
+            double probability = Math.random();
+            if(probability<=0.6){
+                createObject();
+            }
+        }
+    }
+    
+    //check this
+    @Override
+    public void createObject(){
+        Person spawn = new Joe(getX(), getY(), actors());
+        actors().add(spawn);
+    }
+    
+    @Override
+    public void doSomething(){
+        move();
+        energyLevel();
+        eat();
+        age();
+    }
+    
+    @Override
+    public void energyLevel(){
+        decreaseEnergy();
+        for(int k=0; k<actors().size(); k++){
+            if(actors().get(k).getEnergy()==0){
+                dead();
+            }
+            else if(actors().get(k).getEnergy()<=100){
+                actors().get(k).newColor(Color.DARK_GRAY);
+            }
+        }
+    }
+    
+}
