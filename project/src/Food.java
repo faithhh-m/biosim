30d7a4377e1d22c28d0d4a94940ae15909b4b03f
faithhh-m <85309857+faithhh-m@users.noreply.github.com>
2021-06-11 10:41:45 -0700

    Update Food.java

--- project/src/Food.java
+++ project/src/Food.java
@@ -4,15 +4,19 @@ import java.awt.Color;
 
 public class Food extends Person {
     
-    
-    
-    public Food(double x, double y, ArrayList<Person> foods){
-        super(x, y, foods);
-        
+    public Food(double x, double y, ArrayList<Person> actors){
+        super(x, y, actors);
+        newColor(Color.PINK);
+        changeSize(10);
     }
     
+    @Override
+    public void doSomething(){
+    }
     
-    
-    
-    
+    @Override
+    public void create(Graphics draw){
+        draw.setColor(getColor());
+        draw.fillOval(intX(), intY(), intSize(), intSize());
+    }
 }
